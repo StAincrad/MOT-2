981125ac2cb00e07d507e5591bf9478ff9d05b9b
Amaro Blest <Ablest@ucm.es>
2019-05-29 19:33:12 +0200

    Added : Scene 04, Bullet component and MakeDamage component

---
+++ Practica2/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        BlockBulletConstrains();
+        rb.velocity = transform.right * speed;
+    }
+
+    /// <summary>
+    /// Blocks forces in Y of the bullet
+    /// </summary>
+    private void BlockBulletConstrains()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+    }
+}
