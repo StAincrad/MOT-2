974c22449d5b6cfca163c5d8f47e8d43300d1289
Amaro Blest <Ablest@ucm.es>
2019-05-29 18:55:17 +0200

    Added : Scene 02, PickMe component and GameManager component

---
+++ Practica2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public int playerPoints { get; private set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void AddPoints(int amount)
+    {
+        playerPoints += amount;
+    }
+}
---
+++ Practica2/Assets/Scripts/PickMe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickMe : MonoBehaviour
+{
+    public string playerTag;
+    public int points;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(playerTag))
+        {
+            GameManager.instance.AddPoints(points);
+            Destroy(gameObject);
+        }
+    }
+
+}
