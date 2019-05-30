1b37d0049d3fecfbbb77bcd8f1505d39cecbd022
Amaro Blest <Ablest@ucm.es>
2019-05-30 15:43:21 +0200

    Added : Scene 8, sorting layers, layers for collision and FollowTarget component

---
+++ Practica2/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public float distanceY;
+    public Transform target;
+
+	void Update ()
+    {
+        transform.position = new Vector3(target.position.x,target.position.y+distanceY,-10f);
+	}
+}
